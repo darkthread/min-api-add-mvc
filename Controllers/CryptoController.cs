ffa1faa9b6358dc1aa154f498365b7feccc4c1aa
Jeffrey <572199+darkthread@users.noreply.github.com>
2023-12-03 20:27:25 +0800

    Add controller

---
+++ Controllers/CryptoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace min_api_add_mvc.Controllers
+{
+    public class CryptoController : Controller
+    {
+        [HttpPost]
+        public IActionResult Sha1(string data)
+        {
+            var hash = BitConverter.ToString(
+                    SHA1.Create().ComputeHash(
+                        Encoding.UTF8.GetBytes(data.ToString())))
+                .Replace("-", string.Empty);
+            return Content($"<pre>{hash}</pre>", "text/html");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 啟用 Controller
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // 指定 index.html 為 wwwroot 預設文件
@@ -10,14 +13,8 @@ app.UseDefaultFiles();
 // 啟用 wwwroot 靜態檔資料夾
 app.UseFileServer();
 
-app.MapPost("/Sha1", async (HttpContext ctx) =>
-{
-    var hash = BitConverter.ToString(
-        SHA1.Create().ComputeHash(
-            Encoding.UTF8.GetBytes(ctx.Request.Form["data"].ToString())))
-    .Replace("-", string.Empty);
-    ctx.Response.ContentType = "text/html";
-    await ctx.Response.WriteAsync($"<pre>{hash}</pre>");
-});
+// 指定 Controller 路由
+app.MapControllers();
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
